08fe4a8ed2899cb9dec349288134b133ad096367
Andy Mam <118705791+andymam@users.noreply.github.com>
2025-01-31 13:01:09 -0700

    Achieve 80% line coverage

--- src/routes/orderRouter.test.js
+++ src/routes/orderRouter.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const app = require('../service');
+const { StatusCodeError } = require('../endpointHelper')
 const { expectValidJwt, randomName, createAdminUser } = require('./testUtils');
 
 if (process.env.VSCODE_INSPECTOR_OPTIONS) {
@@ -24,4 +25,33 @@ test('get menu', async () => {
         expect(pizza).toHaveProperty('title');
         expect(pizza).toHaveProperty('image');
     });
+});
+
+test('add menu item', async () => {
+    const newMenuItem = { "title":"Student", "description": "No topping, no sauce, just carbs", "image":"pizza9.png", "price": 0.0001 };
+    const res = await request(app).put('/api/order/menu')
+    .set('Authorization', `Bearer ${adminAuthToken}`)
+    .send(newMenuItem);
+    expect(res.status).toBe(200);
+    expect(Array.isArray(res.body)).toBe(true);
+    expect(res.body.some(item => item.title === "Student")).toBe(true);
+});
+
+test('create order', async () => {
+    const order = {"franchiseId": 1, "storeId":1, "items":[{ "menuId": 1, "description": "Veggie", "price": 0.05 }]};
+    const res = await request(app).post('/api/order')
+    .set('Authorization', `Bearer ${adminAuthToken}`)
+    .send(order);
+    expect(res.status).toBe(200);
+    expect(res.body).toHaveProperty('order');
+    expect(res.body).toHaveProperty('jwt');
+    expect(res.body.order.items[0]).toHaveProperty('price', 0.05)
+});
+
+
+test('StatusCodeError should store message and statusCode', () => {
+    const error = new StatusCodeError('Test Error', 404);
+    expect(error).toBeInstanceOf(Error);
+    expect(error.message).toBe('Test Error');
+    expect(error.statusCode).toBe(404);
 });
