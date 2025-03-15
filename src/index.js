f2bdbf2f3a2af94bf2021bcdf147d20f2b7c34d3
Andy Mam <118705791+andymam@users.noreply.github.com>
2025-03-15 13:56:36 -0600

    HTTP requests

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ const { sendMetricsPeriodically } = require('./metrics');
 const port = process.argv[2] || 3000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
-  sendMetricsPeriodically(1000);
+  sendMetricsPeriodically(10000);
 });
